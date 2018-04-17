f8bc0e097de45dd4ad1a75ebcd1e3c29b8da0d4f
Thomas Shaw <shawtr@umich.edu>
2018-04-17 17:42:30 -0400

    Rewrite for...
    -arbitrary number of channels
    -slightly different imagestruct structure
    -including dataset referenced by filename

--- imerge_from_imagestruct.m
+++ imerge_from_imagestruct.m
@@ -1,27 +1,74 @@
 function [Imerge, ir] = imerge_from_imagestruct(istruct)
 
-ir = default_iref(istruct.data{1}, istruct.psize);
 
-d1 = istruct.data{1};
-d2 = istruct.data{2};
+ds = load(istruct.data_fname);
 
-I1 = reconstruct(d1, ir);
-I2 = reconstruct(d2, ir);
+if numel(ds.data) ~= istruct.channels
+    error('imerge_from_imagestruct: data has different number of channels from imagestruct');
+end
 
-% sigmas need to be in units of reconstruction pixels
-sigma1 = istruct.sigmablur{1}/istruct.psize; 
-sigma2 = istruct.sigmablur{2}/istruct.psize;
+if ~strcmp(ds.units, istruct.units)
+    error('imerge_from_imagestruct: data has different units from imagestruct');
+end
 
-PSF1 = 2*pi*sigma1^2*fspecial('gaussian', ceil(4*sigma1), sigma1);
-PSF2 = 2*pi*sigma2^2*fspecial('gaussian', ceil(4*sigma2), sigma2);
+if isfield(istruct, 'imageref')
+    ir = istruct.imageref;
+else
+    ir = default_iref(ds.data{1}, istruct.psize);
+end
 
-Iblur1 = imfilter(I1, PSF1, 'replicate');
-Iblur2 = imfilter(I2, PSF2, 'replicate');
+cm_size = 64;
+cm = gray(cm_size);
+Imerge = zeros(ir.ImageSize);
+% Make images for each channel
+for i = 1:istruct.channels
+    d = ds.data{i};
 
-clim1 = istruct.clim{1};
-clim2 = istruct.clim{2};
+    I = reconstruct(d, ir);
 
-If1 = (Iblur1 - clim1(1))/(diff(clim1));
-If2 = (Iblur2 - clim2(1))/(diff(clim2));
+    % sigmas need to be in units of reconstruction pixels
+    sigma = istruct.sigmablur(i)/istruct.psize; 
 
-Imerge = cat(3, If1, If2, If1);
+    PSF = 2*pi*sigma^2*fspecial('gaussian', ceil(4*sigma), sigma);
+
+    Iblur = imfilter(I, PSF, 'replicate');
+
+    cmin = istruct.cmin(i);
+    cmax = istruct.cmax(i);
+
+    % scale to a color
+    col = col2rgb(istruct.color{i});
+
+    If{i} = ind2rgb( 1 + uint8((cm_size - 1)*(Iblur - cmin)/(cmax - cmin)), ...
+                        cm*diag(col));
+
+    Imerge = Imerge + If{i};
+end
+
+
+function rgb = col2rgb(col)
+if isnumeric(col) && size(col) == [1 3]
+    rgb=col;
+else
+    if ~ischar(col)
+        error('col2rgb: col must be a string or a 1x3 matrix');
+    end
+    switch col
+        case {'y', 'yellow'}
+            rgb = [1 1 0];
+        case {'m', 'magenta'}
+            rgb = [1 0 1];
+        case {'c', 'cyan'}
+            rgb = [0 1 1];
+        case {'r', 'red'}
+            rgb = [1 0 0];
+        case {'g', 'green'}
+            rgb = [0 1 0];
+        case {'b', 'blue'}
+            rgb = [0 0 1];
+        case {'w', 'white'}
+            rgb = [1 1 1];
+        case {'k', 'black'}
+            rgb = [0 0 0];
+    end
+end
