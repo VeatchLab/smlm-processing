dbfc927b7e18bcd1accac45ffb3a743255610d37
Thomas Shaw <shawtr@umich.edu>
2018-04-30 08:45:25 -0400

    Add options for new_to_old for compatibility...
    with later imagedata structures

--- compat/new_to_old.m
+++ compat/new_to_old.m
@@ -1,17 +1,26 @@
-function olddata = new_to_old(newdata)
+function olddata = new_to_old(newdata, data_fieldname, transposex_flag)
 % Turn old STORM_analyzer style struct-of-struct data
 % into new style 2d struct array
 
+if nargin < 2
+    data_fieldname = 'data';
+end
+if nargin < 3
+    transposex_flag = true;
+end
+
 [nmovies,nframes] = size(newdata);
 
-olddata = repmat(struct('data',[]),1,nmovies);
+olddata = repmat(struct(data_fieldname,[]),1,nmovies);
 
 for imov = 1:nmovies
     olddata(imov).data = newdata(imov,:);
     for iframe = 1:nframes
         n = numel(newdata(imov,iframe).x);
-        olddata(imov).data(iframe).x = reshape(newdata(imov,iframe).y, n,1);
-        olddata(imov).data(iframe).y = reshape(newdata(imov,iframe).x, n,1);
+        if transposex_flag
+            olddata(imov).(data_fieldname)(iframe).x = reshape(newdata(imov,iframe).y, n,1);
+            olddata(imov).(data_fieldname)(iframe).y = reshape(newdata(imov,iframe).x, n,1);
+        end
     end
 end
 
