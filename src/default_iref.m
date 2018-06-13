07c92a4870b0e9410b42edffa406ad6f361871c4
Thomas Shaw <shawtr@umich.edu>
2018-06-13 15:58:05 -0400

    Make default_iref more generic in 'data' arg

--- src/default_iref.m
+++ src/default_iref.m
@@ -1,19 +1,32 @@
-function iref = default_iref(data, psize, range)
+function iref = default_iref(data, psize)
 
-
-xs = [data.x];
-ys = [data.y];
-
-if nargin<3
-    minx = min(xs);
-    miny = min(ys);
-    pwidth = ceil((max(xs) - minx)/psize);
-    pheight = ceil((max(ys) - miny)/psize);
+if isnumeric(data)
+    if size(data) == [1 4] % user gave a window
+        dtype = 'window';
+    elseif size(data,2) == 2
+        dtype = 'points';
+        xs = data(:,1)';
+        ys = data(:,2)';
+    end
 else
-    minx = range(1);
-    miny = range(3);
-    pwidth = ceil((range(2) - minx)/psize);
-    pheight = ceil((range(4) - miny)/psize);
+    dtype = 'points';
+    xs = [data.x];
+    ys = [data.y];
+end
+
+switch dtype
+    case 'window'
+        range = data;
+
+        minx = range(1);
+        miny = range(3);
+        pwidth = ceil((range(2) - minx)/psize);
+        pheight = ceil((range(4) - miny)/psize);
+    case 'points'
+        minx = min(xs);
+        miny = min(ys);
+        pwidth = ceil((max(xs) - minx)/psize);
+        pheight = ceil((max(ys) - miny)/psize);
 end
 
 width = pwidth*psize;
