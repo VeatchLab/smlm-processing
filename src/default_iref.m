2810efc6215f934162ffc0e3f6984a41421d468f
Thomas Shaw <shawtr@umich.edu>
2018-06-13 15:46:58 -0400

    SLV added an optional argument to default_iref
    which specifies a box (rather than taking min to max of points)

--- src/default_iref.m
+++ src/default_iref.m
@@ -1,12 +1,20 @@
-function iref = default_iref(data, psize)
+function iref = default_iref(data, psize, range)
+
 
 xs = [data.x];
 ys = [data.y];
 
-minx = min(xs);
-miny = min(ys);
-pwidth = ceil((max(xs) - minx)/psize);
-pheight = ceil((max(ys) - miny)/psize);
+if nargin<3
+    minx = min(xs);
+    miny = min(ys);
+    pwidth = ceil((max(xs) - minx)/psize);
+    pheight = ceil((max(ys) - miny)/psize);
+else
+    minx = range(1);
+    miny = range(3);
+    pwidth = ceil((range(2) - minx)/psize);
+    pheight = ceil((range(4) - miny)/psize);
+end
 
 width = pwidth*psize;
 height = pheight*psize;
