d564ab8d80c92d2770c661de2423c760137daba0
Thomas Shaw <shawtr@umich.edu>
2018-07-16 15:18:29 -0400

    Make run_batch_fitting do more
    Also wrap transform code in transform_block(), which is
    convenient for applying a transform to an SP type dataset
    with a record.

--- src/run_batch_fitting.m
+++ src/run_batch_fitting.m
@@ -4,15 +4,60 @@ if exist('./record.mat', 'file')
     return;
 end
 record = SP_record_default(nchannels, varargin{:});
+save record.mat -struct record % in case something breaks later, save this right away.
 
+% Fitting step
 [data, ~, metadata] = STORMprocess(record.SPspecs);
+
+record.metadata = metadata;
 
 fits.data = data;
 fits.date = datetime;
 fits.produced_by = 'STORMprocess';
 fits.units = 'px';
 
-save(record.fits_fname, '-struct', 'fits');
+if ~isempty(record.fits_fname)
+    save(record.fits_fname, '-struct', 'fits');
+end
 
-record.metadata = metadata;
+cont = true; % set this false if some failure
+% Transform step
+if cont & ~isempty(record.dv_transform_fname)
+    try
+        transformed = trasform_block(fits, record);
+
+        if ~isempty(record.transformed_fname)
+            save(record.transformed_fname, '-struct', 'transformed');
+        end
+    catch ME
+        warning('An error ocurred in the transform step:\n%s', ME.message);
+        cont = false;
+    end
+end
+
+if cont
+    cspecs = record.SPspecs.camera_specs;
+    dilatefac = cspecs.pixel_size / cspecs.magnification * 1e3; %to nm
+
+    % do to different datasets depending on number of channels
+    if isempty(record.tform_channel)
+        startdata = fits;
+    else
+        startdata = transformed;
+    end
+
+    % apply the dilation to each channel
+    dilateddata = cellfun(@(d) dilatepts(d, dilatefac), startdata.data,...
+                    'UniformOutput', false);
+
+    dilated.data = dilateddata;
+    dilated.date = datetime;
+    dilated.units = 'nm';
+    dilated.produced_by = 'dilatepts';
+
+    if ~isempty(record.dilated_fname)
+        save(record.dilated_fname, '-struct', 'transformed');
+    end
+end
+
 save record.mat -struct record
---
+++ src/transform_block.m
@@ -0,0 +1,47 @@
+function transformed = transform_block(data, record)
+
+transformed = data;
+
+if record.tform_channel % only transform if specified
+
+    transf_fname = record.dv_transform_fname;
+    if exist(transf_fname, 'file')
+        % the transform structures only have reverse transforms,
+        % so we need T_1_2 to transform 2->1
+        tf_mat = load(transf_fname, 'T_1_2', 'Version', 'dims1', 'dims2');
+        tf = tf_mat.T_1_2;
+    else
+        error('dualview transform file not specified or does not exist');
+    end
+
+    % Check consistency of various things
+    % Might want to check Version for something
+    % Check Dims
+    if isfield(tf_mat, 'dims1')
+        msg = 'channel dims for transform are different from those used for fitting';
+        switch record.tform_channel
+            case 1
+                if tf_mat.dims2 ~= record.SPspecs(1).channel_dims
+                    error(msg);
+                end
+            case 2
+                if tf_mat.dims1 ~= record.SPspecs(1).channel_dims || ...
+                        tf_mat.dims2 ~= record.SPspecs(2).channel_dims
+                    error(msg);
+                end
+            otherwise
+                error('I don''t know how to transform that channel yet');
+        end
+    else
+        warning(['Channel dims not specified in transform file. Can''t check for',...
+                    'consistency with fitting specs. Proceed at your own risk']);
+    end
+
+    [tfdata] = apply_transform(data.data{record.tform_channel}, tf);
+
+    transformed.data{record.tform_channel} = tfdata;
+
+    transformed.date = datetime;
+    transformed.produced_by = 'apply_transform';
+    transformed.units = 'px';
+end
