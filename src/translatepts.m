05978684d4d9ef27f0fdfe354c648646b5ef3f2c
Thomas Shaw <shawtr@umich.edu>
2019-03-28 13:45:36 -0400

    optionally shift z in translatepts

--- src/translatepts.m
+++ src/translatepts.m
@@ -1,4 +1,4 @@
-function data = translatepts(indata, xshift, yshift)
+function data = translatepts(indata, xshift, yshift, zshift)
 % TRANSLATEPTS shift a point dataset in space
 %   data = TRANSLATEPTS(indata, xshift, yshift) - move indata.x and indata.y
 %       by the displacements given by xshift and yshift, respectively
@@ -7,9 +7,15 @@ if numel(xshift) ~= 1 || numel(yshift) ~= 1
     error('translatepts: xshift and yshift must be scalar');
 end
 
-data = arrayfun(@(s) translate_once(s, xshift, yshift), indata);
+if nargin<4
+    data = arrayfun(@(s) translate_once(s, xshift, yshift), indata);
+else
+    data = arrayfun(@(s) translate_once(s, xshift, yshift, zshift), indata);
+end
 
-
-function data = translate_once(data, xshift, yshift)
+function data = translate_once(data, xshift, yshift, zshift)
     data.x = data.x + xshift;
     data.y = data.y + yshift;
+    if nargin==4
+        data.z = data.z + zshift;
+    end
